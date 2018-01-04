2e03b6d59ea4f6cecbf41ecca8eca1c909de9bf7
Djwas <33987574+Djwas@users.noreply.github.com>
2018-01-04 12:30:50 +0545

    Add files via upload

---
+++ power4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+long int a,b,c,d;
+for(a=1;a<=99999;a++)
+	{for(b=1;b<=99999;b++)
+		{for(c=1;c<=99999;c++)
+ 			{for(d=1;d<=99999;d++)
+				if ((a*a*a*a)+(b*b*b*b)+(c*c*c*c)==(d*d*d*d)){printf("%li,%li,%li,%li \n",a,b,c,d);}}}}
+}
